24f8e3dca71c727e7594231dfb74cf472aa7483e
menglinghan <menglinghan@tiaozhan.com>
2020-01-22 16:10:39 +0800

    variable dice

--- src/components/Variable.vue
+++ src/components/Variable.vue
@@ -4,19 +4,32 @@
     <el-popover v-if="editable" trigger="hover">
       <el-input class="hover-input" v-model="input" type="number" :min="1"></el-input>
       <el-button class="hover-button" size="mini" icon="el-icon-minus" circle @click="plus(-input)"></el-button>
+      <el-button class="hover-button" size="mini" icon="el-icon-s-promotion" circle @click="diceOpen(input*1,true)"></el-button>
       <el-button slot="reference" size="mini" icon="el-icon-minus" circle @click="plus(-1)"></el-button>
     </el-popover>
     <b class="value" :style="{color:valueColor}">{{value==null?'--':value}}{{max==null?'':'/'+max}}</b>
     <el-popover v-if="editable" trigger="hover">
       <el-input class="hover-input" v-model="input" type="number" :min="1"></el-input>
       <el-button class="hover-button" size="mini" icon="el-icon-plus" circle @click="plus(input*1)"></el-button>
+      <el-button class="hover-button" size="mini" icon="el-icon-s-promotion" circle @click="diceOpen(input*1,false)"></el-button>
       <el-button slot="reference" size="mini" icon="el-icon-plus" circle @click="plus(1)"></el-button>
     </el-popover>
+    <el-dialog :visible.sync="dialog" width="30%" @closed="diceClose">
+      <div class="check">
+        <dice v-if="dice" :min="1" :max="dice.max" @result="diceFinish"></dice>
+        <b v-if="diceResult!=null" class="check-text" :style="{color:diceResultColor}">{{diceResult.old}} -> {{value}}</b>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import Dice from './Dice'
+
 export default {
+  components: {
+    Dice
+  },
   props: {
     name: String,
     value: Number,
@@ -25,7 +38,10 @@ export default {
   },
   data() {
     return {
-      input: '1'
+      input: '1',
+      dialog: false,
+      dice: null,
+      diceResult: null
     }
   },
   computed: {
@@ -51,15 +67,37 @@ export default {
         }
         return 'green'
       }
+    },
+    diceResultColor() {
+      return this.dice.minus ? 'red' : 'green'
     }
   },
   methods: {
     plus(num) {
       let cur = this.value * 1 + num
       if (this.max != null && cur > this.max) {
-        return
+        cur = this.max
       }
       this.$emit('input', cur)
+    },
+    diceOpen(max, minus) {
+      this.dialog = true
+      this.dice = { max, minus }
+    },
+    diceFinish(e) {
+      this.diceResult = {
+        old: this.value,
+        delta: e
+      }
+      let cur = this.value * 1 + e * (this.dice.minus ? -1 : 1)
+      if (this.max != null && cur > this.max) {
+        cur = this.max
+      }
+      this.$emit('input', cur)
+    },
+    diceClose() {
+      this.dice = null
+      this.diceResult = null
     }
   }
 }
@@ -89,5 +127,16 @@ export default {
 
 .hover-button {
   margin-left: 15px;
+}
+
+.check {
+  display: flex;
+  align-items: center;
+}
+
+.check-text {
+  font-size: 50px;
+  flex-grow: 1;
+  text-align: center;
 }
 </style>
